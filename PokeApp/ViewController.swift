d8d465368e7caee25ddbb93dcf7726f7b33e554e
Alvira Nurhaliza <alvira.wijaya@mncgroup.com>
2021-08-17 09:43:59 +0700

    Initial project

---
+++ PokeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeApp
+//
+//  Created by Alvira Nurhaliza on 17/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
